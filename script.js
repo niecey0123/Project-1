2aa54d74cf81488541f77dbc07446f2c6815aac1
shaniece crumpler <s.crumpler11@gmail.com>
2019-04-04 18:03:29 -0400

    setting a winner

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ window.onload = function() {
 			return `I love this ${this.top} top!`
 		}
 		compliment2(){
-			return `I look good in this ${this.bottoms} bottoms!`
+			return `I look good in this ${this.bottoms}!`
 		}
 		compliment3(){
 			return `This ${this.hair} is sooo me!`
@@ -94,15 +94,18 @@ window.onload = function() {
 		}
 		mission(){
 			return `I need to find an outfit that would look great for my lunch date! You have 30sec to find a ${this.top} top, 
-			some ${this.bottoms} bottoms, and my cool ${this.accessory}.`
+			A ${this.bottoms}, and my cool ${this.accessory}. Hair color ${this.hair}!`
+		}
+		congrats(){
+			return 	`Woo-hoo! You did it! Now That I look Fabolous, We can head Downstairs and Meet Our Date!.`
 		}
 
 	};
 
 
-	let niecey = new avatar("Niecey","Red","Dark Demin","Shades","Blonde");
-	let niecey2 = new avatar("Niecey","White","Cool Grey","Necklace","Black");
-	let niecey3 = new avatar("Niecey","Black","Demin Skirt","Hat","Red");
+	let niecey = new avatar("Niecey","Red","Demin Skirt","Hat","Blonde");
+	let niecey2 = new avatar("Niecey","White","Cool Grey","Necklace","Red");
+	let niecey3 = new avatar("Niecey","Black","Dark Demin","Shades","Black");
 
 
 
@@ -114,6 +117,17 @@ window.onload = function() {
 	let myHair = ['hair-1','hair-2','hair-3'];
 	let headTarget = document.querySelector('#head-point');
 	const hairDiv = document.querySelectorAll('.hStyle')
+
+	let checkObj = {top: '', bottom:'', accessory:'', hair: ''}
+
+	const outfits = [
+	{top: 'Images/crop1.png',bottom:"Images/bottom1.png",accessory:"Images/hat.png",hair:"Images/hairstyle1.png"},
+	{top: 'Images/crop2.png',bottom:"Images/bottom2.png",accessory:"Images/glasses.png",hair:"Images/short-brunnette.png"},
+	{top:'Images/crop3.png',bottom:"Images/bottom3.png",accessory:"Images/necklace.png",hair:"Images/black-long.png"}
+	];
+
+
+
 	const create_hair_style =()=>{
 				for (let x = 0; x < 3; x++) {
 				let hair =document.createElement('div');
@@ -130,27 +144,31 @@ window.onload = function() {
 					headTarget.style.backgroundSize = "cover";
 					headTarget.style.backgroundSize = "200px 200px"
 					headTarget.style.backgroundPosition = "top center";
+					checkObj.hair ="url(images/hairstyle1.png)"
 				
 				}
 				else if (evnt.target === hair2) {
 					textBlock.innerHTML = niecey3.compliment3();
 					headTarget.style.backgroundImage = " url(images/short-brunnette.png)";
 					headTarget.style.backgroundRepeat = "no-repeat";
-					headTarget.style.marginLeft = "28px";
+					headTarget.style.marginLeft = "26 px";
 					headTarget.style.backgroundSize = "cover";
 					headTarget.style.backgroundSize = "145px 100px";
 					headTarget.style.backgroundPosition = "center center";
+					checkObj.hair =" url(images/short-brunnette.png)"
 				}
 				else{
 					textBlock.innerHTML = niecey2.compliment3();
 					headTarget.style.backgroundImage = "url(images/black-long.png)";
 					headTarget.style.backgroundRepeat = "no-repeat";
 					headTarget.style.backgroundSize = "cover"
-					headTarget.style.marginLeft = "25px";
+					headTarget.style.marginLeft = "18px";
 					headTarget.style.marginTop = "20px";
 					headTarget.style.backgroundSize = "120px 220px"
 					headTarget.style.backgroundPosition = "top center";
+					checkObj.hair ="url(images/black-long.png)"
 				}
+				console.log(checkObj)
 	 		});
 		
 	  	};
@@ -173,6 +191,7 @@ window.onload = function() {
 	let myTop=['top1','top2','top3']
 	let topTarget = document.querySelector('#top-point');
 	const topDivs = document.querySelectorAll('.top');
+
 	const create_top_style = () => {
 		for (let y = 0; y < 3; y++) {
 			
@@ -190,24 +209,30 @@ window.onload = function() {
 					topTarget.style.backgroundRepeat = "no-repeat";
 					topTarget.style.backgroundSize = "cover"
 					topTarget.style.backgroundPosition = "center center";
-					// topTarget.style.backgroundImage = "Images/crop1.png"
+					checkObj.top ="url('Images/crop1.png')"
+					console.log(top1)
 				}
 				else if (evnt.target === top2) {
 					textBlock.innerHTML = niecey2.compliment1();
 					topTarget.style.backgroundImage = " url('Images/crop2.png')";
 					topTarget.style.backgroundRepeat = "no-repeat";
-					topTarget.style.backgroundSize = "cover"
+					topTarget.style.backgroundSize = "cover";
+					topTarget.style.marginTop= "10px";
+					topTarget.style.marginLeft= "17px"
 					topTarget.style.backgroundSize = "175px 120px"
 					topTarget.style.backgroundPosition = "top center";
+					checkObj.top =" url('Images/crop2.png')"
 				}
 				else{
 					textBlock.innerHTML = niecey3.compliment1();
 					topTarget.style.backgroundImage = " url('Images/crop3.png')";
 					topTarget.style.backgroundRepeat = "no-repeat";
-					topTarget.style.backgroundSize = "cover"
-					topTarget.style.backgroundSize = "250px 300px"
+					topTarget.style.backgroundSize = "cover";
+					topTarget.style.backgroundSize = "250px 300px";
 					topTarget.style.backgroundPosition = "top center";
+					checkObj.top =" url('Images/crop3.png')"
 				}
+				console.log(checkObj)
 	
 	 		});
 		};
@@ -230,6 +255,8 @@ window.onload = function() {
  	let myBottom = ['bottom1','bottom2','bottom3'];
  	let bottomTarget = document.querySelector('#bottom-point');
  	const midDivs = document.querySelectorAll('.bottom');
+ 
+
  	const create_bottom = ()=>{
  		for (let z = 0; z < 3; z++) {
  			
@@ -247,14 +274,19 @@ window.onload = function() {
 					bottomTarget.style.backgroundRepeat = "no-repeat";
 					bottomTarget.style.backgroundSize = "cover"
 					bottomTarget.style.backgroundPosition = "center center";
+					checkObj.bottom ="url('Images/bottom1.png')"
+
 				}
 				else if (evnt.target === bottom2) {
 					textBlock.innerHTML = niecey2.compliment2();
 					bottomTarget.style.backgroundImage = " url('Images/bottom2.png')";
 					bottomTarget.style.backgroundRepeat = "no-repeat";
+					bottomTarget.style.marginTop= "3px"
+					bottomTarget.style.marginLeft= "3px"
 					bottomTarget.style.backgroundSize = "cover"
 					bottomTarget.style.backgroundSize = "175px 120px"
 					bottomTarget.style.backgroundPosition = "top center";
+					checkObj.bottom ="url('Images/bottom2.png')"
 				}
 				else{
 					textBlock.innerHTML = niecey3.compliment2();
@@ -263,8 +295,9 @@ window.onload = function() {
 					bottomTarget.style.backgroundSize = "cover"
 					bottomTarget.style.backgroundSize = "160px 170px"
 					bottomTarget.style.backgroundPosition = "top center";
+					checkObj.bottom ="url('Images/bottom3.png')"
 				}
-	
+				console.log(checkObj)
 	
 	 		});
  		};
@@ -286,9 +319,12 @@ window.onload = function() {
 	let midTarget = document.querySelector('#mid-point');
 	let neckTarget = document.querySelector('#neck');
 	const lastDivs = document.querySelectorAll('.thing');
+
+
+
 	const create_accessories = ()=>{
 		for (let a = 0; a < 3 ; a++) {
-			
+				
 				let last_layer = document.createElement('div');
 				last_layer.className= myAccessories[a];
 				lastDivs[a].appendChild(last_layer);
@@ -305,28 +341,38 @@ window.onload = function() {
 					midTarget.style.backgroundSize = "100px 100px"
 					midTarget.style.backgroundSize = "cover"
 					midTarget.style.backgroundPosition = "top center";
+					// checkOutfit(thing1.style.backgroundImage)
+					checkObj.accessory = "url('Images/hat.png')"
 				}
 				else if (evnt.target === thing2) {
 					textBlock.innerHTML = niecey.compliment4();
-					midTarget.style.backgroundImage = " url('Images/glasses.png')";
+					midTarget.style.backgroundImage = "url('Images/glasses.png')";
 					midTarget.style.backgroundRepeat = "no-repeat";
 					midTarget.style.backgroundSize = "cover"
 					midTarget.style.backgroundSize = "70px 50px"
-					midTarget.style.marginTop = "30px";
+					midTarget.style.marginTop = "39px";
 					midTarget.style.backgroundPosition = "center center";
+					checkObj.accessory = "url('Images/glasses.png')"
 				}
+
 				else if (evnt.target === thing3){
 					textBlock.innerHTML = niecey2.compliment4();
-					neckTarget.style.backgroundImage = " url('Images/necklace.png')";
+					neckTarget.style.backgroundImage = "url('Images/necklace.png')";
 					neckTarget.style.backgroundRepeat = "no-repeat";
-					neckTarget.style.backgroundSize = "cover"
+					neckTarget.style.backgroundSize = "cover";
+					midTarget.style.marginLeft = "30px";
 					neckTarget.style.backgroundSize = "70px 70px"
 					neckTarget.style.backgroundPosition = "top center";
+					checkObj.accessory = "url('Images/necklace.png')"
 				}
-	
+			console.log(checkObj)
 	 		});
+
 		};
 	};
+
+
+	console.log(checkObj)
 	create_accessories();
 
 	let thing1 = document.querySelector('.thing1')
@@ -349,42 +395,39 @@ window.onload = function() {
 				clearInterval(time);
 				document.getElementById('timer').innerText = 'Ready!';
 				document.getElementById('timer').style.backgroundColor = "#FDFF47";
+				
 			};
 		},1000);
 	}
 	
+	
 
-	// console.log(`top:` + top1.clientTop + "px" + "border");
-	// console.log(`left:` + top1.offsetLeft + "px" + "position and margin of element");
-	// console.log(`border:` + top1.offsetHeight / 2 + "px");
-	// console.log(top1.offsetWidth / 2 + "px");
-	// class avatar {
-	// 	constructor(name,top,bottoms,accessory){
-	// 		this.name = name;
-	// 		this.top = top;
-	// 		this.bottoms = bottoms;
-	// 		this.accessory = accessory;
-			
-	// 	};
-	// 	welcomeGreeting(){
-	// 	return `Hi!, I'm ${this.name}. Welcome to my room! I have a date in a few and I need your help. Can you get me ready in time?`;
-	// 	};
-	// 	compliment1(){
-	// 		return `${this.top} top!`
-	// 	}
-	// 	mission(){
-	// 		return `I need to find an outfit that would look great for my lunch date! You have 30sec to find a ${this.top} top, 
-	// 		some ${this.bottoms} bottoms, and my cool ${this.accessory}.`
-	// 	}
+	// let arr =[];
 
+	// const check = ()=>{
+	// 	for (let y = 0; y < outfits.length; y++) {
+	// 	console.log(outfits[y])
+	// 	};
 	// };
+	// check();
 
+	 // const checkOutfit = ()=>{
+	 // 	if (arr.length === 4) {
+	 // 		for (let i = 0; i < arr.length; i++) {
+		//  		console.log(arr[i])
+		//  	}
+	 // 	}
+	 	
+	 // }
 
-	// let niecey = new avatar("Niecey","Black","Dark Demin","Shades",);
-	// 	textBlock.innerHTML = niecey.welcomeGreeting();
-	// 	setTimeout(()=>{textBlock.innerHTML = niecey.mission()},4000);
-	// 	setTimeout(()=>{countdown()},6000);
-	// 	setTimeout(()=>{textBlock.style.display = "none"},10000);
+   // let winnings =()=>{
+   // 	if (top1 && bottom1 && hair1  && thing1) {
+   // 		return textBlock.innerHTML = niecey.congrats();
+   // 	}
+   // }
+   // winnings();
+   
+
 		
 };
 
